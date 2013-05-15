f51e6c5d6066db063a052483772b3c2296db4f36
Diek W. Wheeler <dwheele5@gmu.edu>
2013-05-15 18:27:54 -0400

    update to relase notes for v.10 R 3A

--- Help_Release_Notes.php
+++ Help_Release_Notes.php
@@ -121,8 +121,10 @@ ul
 <p class=MsoNormal><b><u><span style='font-size:16.0pt;line-height:115%;
 font-family:"Arial","sans-serif"'>Release Notes:</span></u></b></p>
 
+<!--
 <p class=MsoNormal><span style='font-size:16.0pt;line-height:115%;
 font-family:"Arial","sans-serif"'>v1.0&alpha; R 3A:</span></p>
+-->
 
 <p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
 style='font-size:14.0pt;line-height:115%;font-family:Symbol'>·<span
@@ -130,6 +132,11 @@ style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 </span></span><span style='font-size:14.0pt;line-height:115%;font-family:"Arial","sans-serif"'>In marker matrix, "no information available" squares are no longer linked to an (empty) evidence page.
 (14 May 2013)
 </span></p>
+
+<BR><BR>
+
+<p class=MsoNormal><span style='font-size:16.0pt;line-height:115%;
+font-family:"Arial","sans-serif"'>v1.0&alpha; R 3A:</span></p>
 
 <p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
 style='font-size:14.0pt;line-height:115%;font-family:Symbol'>·<span
