5631b7acdf0883abc7c3a430dc42a0fdf86a771a
Diek W. Wheeler <dwheele5@gmu.edu>
2013-05-21 17:29:27 -0400

    fixes #117

--- Help_Release_Notes.php
+++ Help_Release_Notes.php
@@ -129,7 +129,7 @@ font-family:"Arial","sans-serif"'>v1.0&alpha; R 3A:</span></p>
 <p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
 style='font-size:14.0pt;line-height:115%;font-family:Symbol'>·<span
 style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</span></span><span style='font-size:14.0pt;line-height:115%;font-family:"Arial","sans-serif"'>Fixed the missing connectivity information on the Neuron pages.
+</span></span><span style='font-size:14.0pt;line-height:115%;font-family:"Arial","sans-serif"'>Replaced missing code, which fixes the missing connectivity information on the Neuron pages.
 (21 May 2013)
 </span></p>
 
