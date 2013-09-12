df24f5765f5825160100950ed56795a06ec70228
Christopher Rees <crees@gmu.edu>
2013-09-12 16:20:40 -0400

    new header graphic (issue 62); unifies header style and options across all pages; improves use of space on pages (issue 71); gives users more control over opening new tabs (issue 73)

---
+++ function/menu_main.php
@@ -0,0 +1,43 @@
+<%@LANGUAGE="JAVASCRIPT" CODEPAGE="1252"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+
+<head>
+<link rel="stylesheet" href="function/menu_support_files/menu_main_style.css" type="text/css" />
+<title></title>
+</head>
+
+<body>
+
+	<div id="menu_main_button_new_clr">
+
+	<ul id="css3menu0" class="topmenu">
+		<li class="topfirst"><a href="morphology.php" style="height:32px;line-height:32px;"><span><img src="function/menu_support_files/news.png" alt=""/>Browse</span></a>
+	
+		<ul>
+	
+			<li class="subfirst"><a href="morphology.php">Morphology</a></li>
+			<li><a href="markers.php">Molecular markers</a></li>
+			<li><a href="ephys.php">Electrophysiology</a></li>
+			<li><a href="connectivity.php">Connectivity</a></li>
+	
+		</ul></li>
+	
+		<li class="topmenu"><a href="search.php?searching=1" style="height:32px;line-height:32px;"><span><img src="function/menu_support_files/find.png" alt=""/>Search</span></a>
+	
+		<ul>
+	
+			<li class="subfirst"><a href="search.php?searching=1">Neuron type</a></li>
+			<li><a href="find_author.php?searching=1">Author</a></li>
+			<li><a href="find_pmid.php?searching=1">PMID/ISBN</a></li>
+	
+		</ul></li>
+	
+		<li class="toplast"><a href="help.php" style="height:32px;line-height:32px;"><img src="function/menu_support_files/help.png" alt=""/>Help</a></li>
+	
+	</ul>
+	
+	</div>  
+
+</body>
+</html>
