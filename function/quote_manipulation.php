bbb6de57e073b9e14621e8833741855d38c69f4d
Christopher Rees <crees@gmu.edu>
2013-09-29 14:34:43 -0400

    Separates mixed expressions on neuron pages (issue 144), parses quotes and adds type names (issue 130), and updates brain logo image

---
+++ function/quote_manipulation.php
@@ -0,0 +1,30 @@
+<?php
+
+function quote_replaceIDwithName($theQuote)
+{
+	// find all <% cell ID %>
+	preg_match_all('/\<% ([^<>]+) %\>/', $theQuote, $matches, PREG_PATTERN_ORDER);	
+		
+	$u = 0;	
+	foreach ($matches[1] as $match) {
+		$idArray[] = $match;
+		
+		$fetch_sub_nick_by_id="SELECT DISTINCT subregion, nickname FROM type WHERE id=$match;";
+		$query_result = mysql_query($fetch_sub_nick_by_id);				
+
+		$subs_and_nicks = mysql_fetch_array($query_result, MYSQL_ASSOC);
+		//$subs[$u] = $subs_and_nicks['subregion'];
+		//$nicks[$u] = $subs_and_nicks['nickname'];
+		$printable_subs_and_nicks[$u] = '{' . $subs_and_nicks['subregion'] . ' ' . $subs_and_nicks['nickname'] . '}';
+
+		$u++;		
+	}
+	
+	// replace <% cell ID %> with {cell type name} by using regular expression search
+	$newQuote = preg_replace_callback('/\<% [^<>]+ %\>/', function($matches) use (&$printable_subs_and_nicks) {
+		return array_shift($printable_subs_and_nicks);
+	}, $theQuote);
+	
+	return ($newQuote);
+}
+?>
