9c67695c129e95cb37cf20aec394c6c3e69ba2b1
Christopher Rees <crees@gmu.edu>
2013-09-30 00:20:15 -0400

    3rd attempt to fix warning

--- function/quote_manipulation.php
+++ function/quote_manipulation.php
@@ -7,17 +7,18 @@ function quote_replaceIDwithName($theQuote)
 		
 	$u = 0;	
 	foreach ($matches[1] as $match) {
-		$idArray[] = $match;
+		$idArray[] = intval($match);
 		
-		$fetch_query="SELECT c.subregion, c.nickname FROM hippocampome.type c WHERE id=$match";
-		$query_result = mysql_query($fetch_query);		
+		$fetch_query="SELECT subregion, nickname FROM type WHERE id=$match";
+		print_r($fetch_query);
+		$query_result = mysql_query($fetch_query);
 
 		while($subs_and_nicks = mysql_fetch_array($query_result, MYSQL_ASSOC)) {
 			//$subs[$u] = $subs_and_nicks['subregion'];
 			//$nicks[$u] = $subs_and_nicks['nickname'];
 			$printable_subs_and_nicks[$u] = '{' . $subs_and_nicks['subregion'] . ' ' . $subs_and_nicks['nickname'] . '}';
-		}
-		$u++;
+			$u++;
+		}		
 	}
 	
 	// replace <% cell ID %> with {cell type name} by using regular expression search
