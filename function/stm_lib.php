3bb0eee054c9df1c9694ddf5f1190b13344115f3
unknown <rjohn3@masonlive.gmu.edu>
2014-02-20 13:24:51 -0500

    stm_lib.php

--- function/stm_lib.php
+++ function/stm_lib.php
@@ -34,7 +34,10 @@ function get_sorted_records($type_ids) {  // used to sort type records
 
 // returns a set of type_ids that have either axons or dendrites in an array of parcels
 function filter_types_by_morph_property($axon_dendrite, $parcel_array) {
-  global $morphology_properties_query;
+  $morphology_properties_query = "SELECT DISTINCT t.name, t.subregion, t.nickname, p.subject, p.predicate, p.object, eptr.Type_id, eptr.Property_id
+      FROM EvidencePropertyTypeRel eptr
+      JOIN (Property p, Type t) ON (eptr.Property_id = p.id AND eptr.Type_id = t.id)
+      WHERE predicate = 'in' AND object REGEXP ':'";
   $quoted_parcels = array_map("quote_for_mysql", $parcel_array);
   $query = $morphology_properties_query . " AND status = 'active' AND subject = '$axon_dendrite'" . " AND object IN " . '(' . implode(', ', $quoted_parcels) . ')';
   //print "<br><br>TYPE FILTERING QUERY:<br>"; print_r($query);
