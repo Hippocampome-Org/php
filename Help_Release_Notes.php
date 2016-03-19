a27d044dd17eb4c032c6ef5ca8c76a679bcec660
hippocampome <hippocampome@gmail.com>
2016-03-19 09:09:25 -0400

    Fixed Issue 030 - New php code to interpret AIS or perisomatic-targeting tags with resultant dynamically generated connectivity matrix

--- Help_Release_Notes.php
+++ Help_Release_Notes.php
@@ -97,6 +97,17 @@ ul
 font-family:"Arial","sans-serif"'>Release Notes:</span></u></b></p>
 
 <p class=MsoNormal><span style='font-size:16.0pt;line-height:115%;
+font-family:"Arial","sans-serif"'>v1.0 R 1H (19 Mar 2016):</span></p>
+
+<p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
+style='font-size:14.0pt;line-height:115%;font-family:Symbol'>·<span
+style='font:7.0pt "Times New Roman"'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span></span><span style='font-size:14.0pt;line-height:115%;font-family:"Arial","sans-serif"'>Fixed Issue 030: New php code to interpret AIS or perisomatic-targeting tags with resultant dynamically generated connectivity matrix.
+</span></p>
+
+</BR>
+
+<p class=MsoNormal><span style='font-size:16.0pt;line-height:115%;
 font-family:"Arial","sans-serif"'>v1.0 R 1G (18 Mar 2016):</span></p>
 
 <p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
