6df6da0e546841ed773af6901d246067c1478572
hippocampome <hippocampome@gmail.com>
2018-02-01 18:53:40 -0500

    merged branch pseudocode-update

--- Help_Release_Notes.php
+++ Help_Release_Notes.php
@@ -97,7 +97,7 @@ ul
 font-family:"Arial","sans-serif"'>Release Notes:</span></u></b></p>
 
 <p class=MsoNormal><span style='font-size:16.0pt;line-height:115%;
-font-family:"Arial","sans-serif"'>v1.3 R 1E (31 Jan 2018):</span></p>
+font-family:"Arial","sans-serif"'>v1.3 R 1E (1 Feb 2018):</span></p>
 
 <p class=MsoListParagraphCxSpFirst style='text-indent:-.25in'><span
 style='font-size:14.0pt;line-height:115%;font-family:Symbol'>&#8226;<span
