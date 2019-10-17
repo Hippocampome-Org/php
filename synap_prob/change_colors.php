4fa247115a4e8e1643dcbfb83a738b928c051e62
Nate Sutton <natemsutton@gmail.com>
2019-10-17 19:15:26 -0400

    added parcel categories to potential synapses display

--- synap_prob/change_colors.php
+++ synap_prob/change_colors.php
@@ -35,25 +35,5 @@
 			document.getElementById(old_row_border_change).style.backgroundImage='';    
 			document.getElementById(old_row_border_change).style.backgroundColor='white';
 		}
-	}
-	function changebordercolor2(curRow)
-	{
-		old_border_change = border_change;
-		border_change=curRow;
-		var row_border_change = 'main_table_row_'+border_change;
-		var old_row_border_change = 'main_table_row_'+old_border_change;
-		/* change row border */
-		if (border_change != selected_row) {
-			document.getElementById(row_border_change).style.backgroundImage='linear-gradient(white 0%, white 94%, red 100%)'; 
-			document.getElementById(row_border_change).style.backgroundImage='-moz-linear-gradient(white 0%, white 85%, red 100%)'; 
-			document.getElementById(row_border_change).style.backgroundImage='-webkit-linear-gradient(white 0%, white 94%, red 100%)';
-			document.getElementById(row_border_change).style.backgroundImage='-ms-linear-gradient(white 0%, white 85%, red 100%)';
-			document.getElementById(row_border_change).style.backgroundImage='-o-linear-gradient(white 0%, white 85%, red 100%)';
-		}
-		/* change old row border back */
-		/*if (old_border_change != selected_row) {
-			document.getElementById(old_row_border_change).style.backgroundImage='';    
-			document.getElementById(old_row_border_change).style.backgroundColor='white';
-		}*/
 	}	
 </script>
