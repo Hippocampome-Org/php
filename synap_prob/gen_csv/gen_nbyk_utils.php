600b9990fd856fc2131c4446664060b6fedca8bf
Nate Sutton <natemsutton@gmail.com>
2020-02-23 20:22:18 -0500

    synpro data now in seperate tables

---
+++ synap_prob/gen_csv/gen_nbyk_utils.php
@@ -0,0 +1,33 @@
+<?php
+	function neurite_to_neuriteID($prop_parcel_rel, $neurite) {
+		$neurite_id='';
+		for ($i=0;$i<count($prop_parcel_rel);$i++) {
+			// case insensitive match used here
+			if (strtolower($neurite)==strtolower($prop_parcel_rel[$i][1])) {
+				$neurite_id=$prop_parcel_rel[$i][0];
+				//echo $neurite.' '.$prop_parcel_rel[$i][1].'<br>';
+			}
+		}
+		return $neurite_id;
+	}
+
+	function refID_to_fragID($fragments, $refID) {
+		$found=false; // only return the first result found
+		for ($i=0;$i<count($fragments);$i++) {
+			if ($refID==$fragments[$i][1] && $found==false) {
+				$frag_id=$fragments[$i][0];
+				$found=true;
+			}
+		}
+		return $frag_id;
+	}	
+
+	function fragID_to_eviID($evidence, $fragID) {
+		for ($i=0;$i<count($evidence);$i++) {
+			if ($fragID==$evidence[$i][0]) {
+				$evi_id=$evidence[$i][1];
+			}
+		}
+		return $evi_id;
+	}
+?>
