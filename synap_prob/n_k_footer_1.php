333e9d5a29ebaf5b83de96882dcb95e13e9df95b
Nate Sutton <natemsutton@gmail.com>
2020-03-27 14:14:52 -0400

    removed netlist links

--- synap_prob/n_k_footer_1.php
+++ synap_prob/n_k_footer_1.php
@@ -4,11 +4,6 @@
     <table border="0" width="180" cellspacing="5">
       <tr height="50">
         <td colspan="2" style="text-align:center"><font class='font7' style='font-size: 20px;'>Download</font></td>
-      </tr>
-      <tr height="20">
-        <td style="float:right"><a href="synap_prob/data/netlist_nk.csv"><img id="csvCN" src='synap_prob/media/ExportCSV.png' width="30px" border="0"/></a></td>
-        <td><span style="position:relative;right:13px"><font class='font5'>Netlist</font></span></td>
-        <td></font></td> 
-      </tr>    
+      </tr>   
       <tr height="20">
         <td style="float:right">
