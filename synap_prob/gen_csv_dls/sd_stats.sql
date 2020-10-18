23f0a71daab311f79342847d2bb629171248e510
Nate Sutton <natemsutton@gmail.com>
2020-10-18 13:51:36 -0400

    updated min and max

--- synap_prob/gen_csv_dls/sd_stats.sql
+++ synap_prob/gen_csv_dls/sd_stats.sql
@@ -1,15 +1,14 @@
 CREATE VIEW SynProSDStats AS SELECT 
-#hippocampome_neuronal_class, unique_id, neurite, neurite_id, 
-        GROUP_CONCAT(DISTINCT hippocampome_neuronal_class) as hippocampome_neuronal_class,
-        GROUP_CONCAT(DISTINCT unique_id) as unique_id,
-        GROUP_CONCAT(DISTINCT neurite) as neurite,
-        GROUP_CONCAT(DISTINCT neurite_id) as neurite_id,
-CAST(STD(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS std_sd, 
-CAST(AVG(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS avg, 
-CAST(COUNT(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS count_sd, 
-CAST(AVG(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS avg_trunk, 
-CAST(MIN(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS min_sd, 
-CAST(MAX(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS max_sd
+	GROUP_CONCAT(DISTINCT hippocampome_neuronal_class) as hippocampome_neuronal_class,
+	GROUP_CONCAT(DISTINCT unique_id) as unique_id,
+	GROUP_CONCAT(DISTINCT neurite) as neurite,
+	GROUP_CONCAT(DISTINCT neurite_id) as neurite_id,
+	CAST(STD(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS std_sd, 
+	CAST(AVG(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS avg, 
+	CAST(COUNT(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS count_sd, 
+	CAST(AVG(CAST(avg_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS avg_trunk, 
+	CAST(MIN(CAST(min_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS min_sd, 
+	CAST(MAX(CAST(max_path_length AS DECIMAL(10,2))) AS DECIMAL(10,2)) AS max_sd
 FROM neurite_quantified 
 WHERE avg_path_length!='' AND avg_path_length!=0 AND neurite NOT LIKE '%:All:A'
 AND neurite NOT LIKE '%:All:D'
--- synap_prob/gen_csv_dls/sd_values.sql
+++ synap_prob/gen_csv_dls/sd_values.sql
@@ -8,15 +8,6 @@ SELECT
     count_sd AS somatic_distance_values_count,
     min_sd AS somatic_distance_min,
     max_sd AS somatic_distance_max
-    #GROUP_CONCAT(DISTINCT SynProSDStats.hippocampome_neuronal_class) AS neuron,
-    #GROUP_CONCAT(DISTINCT unique_id),
-    #GROUP_CONCAT(DISTINCT SynProSDStats.neurite) AS parcel,
-    #GROUP_CONCAT(DISTINCT neurite_id),
-    #GROUP_CONCAT(DISTINCT SynProSDStats.`avg`) AS somatic_distance_avg,
-    #GROUP_CONCAT(DISTINCT std_sd) AS somatic_distance_std,
-    #GROUP_CONCAT(DISTINCT count_sd) AS somatic_distance_values_count,
-    #GROUP_CONCAT(DISTINCT min_sd) AS somatic_distance_min,
-    #GROUP_CONCAT(DISTINCT max_sd) AS somatic_distance_max    
 FROM
     SynProSDStats,
     SynproTypeTypeRel,
@@ -24,5 +15,4 @@ FROM
 WHERE
     SynProSDStats.unique_id = SynproTypeTypeRel.type_id
 	AND SynProSDStats.neurite = SynProNetlistParcels.parcel
-    #AND unique_id=4054
 ORDER BY SynproTypeTypeRel.id , SynProNetlistParcels.id;
