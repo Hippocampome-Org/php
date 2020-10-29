7aac48f9c3d3b1bc6be7ee9f82b2fd239ecf3261
Diek W. Wheeler <dwheele5@gmu.edu>
2020-10-29 11:56:06 -0400

    Updated number of rows in morphology files from 122 to 123

--- morphology.php
+++ morphology.php
@@ -469,8 +469,8 @@ $(function(){
  	], 
     //multiselect: true,
    /* pager: '#pager',*/
-    rowNum:122,
-    rowList:[122],
+    rowNum:123,
+    rowList:[123],
    /*  sortname: 'invid',
     sortorder: 'desc',*/
     viewrecords: true, 
